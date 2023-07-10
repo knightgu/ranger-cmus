d65537a244b1d1250d53b1982087327f96dbe864
Yuwen Gu <knightgu34@gmail.com>
2023-07-10 00:06:32 -0400

    Add instant play and pause functions for `cmus`

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-from .cmus import cmus_play, cmus_queue, cmus_lib
+from .cmus import cmus_play, cmus_queue, cmus_lib, cmus_playlist, cmus_pause
--- cmus.py
+++ cmus.py
@@ -11,7 +11,7 @@ def get_files(fm):
         file_objs = selected_files if selected_files else [active_file]
     return file_objs
 
-class cmus_play(Command):
+class cmus_playlist(Command):
     def execute(self):
         """ Add selected files or folders to playlist """
         file_objs = get_files(self.fm)
@@ -44,3 +44,26 @@ class cmus_lib(Command):
         self.fm.notify("Files were sent to cmus library")
         self.fm.thisdir.mark_all(False)
 
+class cmus_play(Command):
+    def execute(self):
+        """ Play selected files or folders """
+        file_objs = get_files(self.fm)
+
+        cmus1 = ["cmus-remote", "-c", "-q"]
+        self.fm.execute_command(cmus1)
+        cmus2 = ["cmus-remote", "-q"]
+        cmus2.extend([f.path for f in file_objs])
+        self.fm.execute_command(cmus2)
+        cmus3 = ["cmus-remote", "-n", "-p"]
+        self.fm.execute_command(cmus3)
+        self.fm.notify("Cmus queue was cleared and files were sent to it.")
+        self.fm.thisdir.mark_all(False)
+
+
+class cmus_pause(Command):
+    def execute(self):
+        """ Toggle pause playing music """
+
+        cmus = ["cmus-remote", "-u"]
+        self.fm.execute_command(cmus)
+        self.fm.thisdir.mark_all(False)
